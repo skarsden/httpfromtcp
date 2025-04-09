78a1dfa3de75a9ca0f1d18fcba94f3d1a2feb31e
skarsden <zachfrancis98@gmail.com>
2025-04-09 13:40:47 -0400

    Separated TCP and UDP functionality

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const port = "localhost:42069"
+
+func main() {
+	address, err := net.ResolveUDPAddr("udp", port)
+	if err != nil {
+		log.Fatalf("Error resolving address")
+	}
+
+	conn, err := net.DialUDP("udp", nil, address)
+	if err != nil {
+		fmt.Errorf("error settting up connection")
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(">")
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+		}
+		_, err = conn.Write([]byte(str))
+		if err != nil {
+			fmt.Printf("error writing '%s' to connection: %s", str, err)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module httpfromtcp
+
+go 1.24.2
+
+require github.com/stretchr/testify v1.10.0 // indirect
